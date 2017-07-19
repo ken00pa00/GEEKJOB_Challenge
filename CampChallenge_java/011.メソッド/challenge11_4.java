33efdb9bcf573e18668260526598cfde1fda58b5
堀口賢太郎 <ken00pa00@gmail.com>
2017-07-19 14:34:53 +0900

    Java_メソッド_戻り値1を提出します。

---
+++ CampChallenge_java/011.メソッド/challenge11_4.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author horiguchikentarou
+ */
+@WebServlet(urlPatterns = {"/challenge11_4"})
+public class challenge11_4 extends HttpServlet {
+
+    boolean myprofile(String name1 , String name2 , String name3 , PrintWriter pw) {
+    pw.println(name1);
+    pw.println(name2);
+    pw.println(name3);
+    return true;
+}
+
+
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        
+            String na1=("私の名前はKです。<br>");
+            String na2=("1994年11月23日生まれです。<br>");
+            String na3=("趣味は卓球です。よろしくお願いします。<br>");
+            
+        if (myprofile(na1,na2,na3,out)) {
+            out.print("この処理は正しく実行できました");
+        } else {
+            out.print("正しく実行できませんでした");
+        }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
