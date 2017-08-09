297f899eb796c3173a0d2b3729a38254f8c1cbf6
堀口賢太郎 <ken00pa00@gmail.com>
2017-08-09 15:56:02 +0900

    Java_標準クラス_タイムスタンプの表示を提出します。

---
+++ CampChallenge_java/015.標準クラス/challenge15_3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author horiguchikentarou
+ */
+public class challenge15_3 {
+    
+    public static void main(String[] args) {
+        
+        Date now = new Date();
+        
+        SimpleDateFormat sdf =
+            new SimpleDateFormat("2016年11月4日 10時0分0秒");
+        System.out.print(sdf.format(now));
+    }  
+}
