29951f78e89cc2f79ee2613578ea52d490cda7a4
堀口賢太郎 <ken00pa00@gmail.com>
2017-08-10 11:28:37 +0900

    Java_標準クラス_バイト数と文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/challenge15_5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author horiguchikentarou
+ */
+public class challenge15_5 {
+    
+    public static void main(String[] args) {
+        String word = "堀口賢太郎";
+        
+        System.out.println("バイト数は、" + word.getBytes().length);
+        System.out.println("文字数は、"  + word.length());
+    }
+}
