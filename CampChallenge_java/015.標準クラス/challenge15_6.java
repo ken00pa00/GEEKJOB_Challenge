11d2a7afe612c0c72e84d9ebddb3ee9b768d72cc
堀口賢太郎 <ken00pa00@gmail.com>
2017-08-10 11:30:32 +0900

    Java_標準クラス_文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/challenge15_6.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author horiguchikentarou
+ */
+public class challenge15_6 {
+    
+    public static void main(String[] args) {
+        String word = "ken00pa00@gmail.com";
+        
+        System.out.println(word.substring(10, 19));
+    }    
+}
